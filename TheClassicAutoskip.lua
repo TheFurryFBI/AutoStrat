1f224c973afee357e37ac4b3740df494a54b5d2e
TheFurryFBI <106680667+TheFurryFBI@users.noreply.github.com>
2023-01-04 20:40:27 +0800

    Update TheClassicAutoskip.lua

--- TheClassicAutoskip.lua
+++ TheClassicAutoskip.lua
@@ -1,18 +1,11 @@
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Sigmanic/ROBLOX/main/ModificationWallyUi')))() --Yes it contains a loadstring, (sorry mm) but it's from Sigmanic so it should be safe (I hope)
 local w = library:CreateWindow("Toggleable Autoskip") -- Creates the window
 w:Toggle("Auto skip") --Toggle name (If you modify this, it doesnt change anything aside from the name of the toggle)
-local ToggleLocalBecauseImLazy = game.CoreGui.ScreenGui.Container["Toggleable Autoskip"]:WaitForChild("container") --Directory to the window, + WaitForChild to prevent those "Invalid member" errors
-local ActualDirectory = ToggleLocalBecauseImLazy.Frame["Auto skip"].Checkmark --Directory to the toggle
+local ActualDirectory = game.CoreGui.ScreenGui.Container["Toggleable Autoskip"]:WaitForChild("container").Frame["Auto skip"].Checkmark --Directory to the window, + WaitForChild to prevent those "Invalid member" errors + to the toggle as well
 
-function CheckForTick() --Actual code
-				local Prop = game.ReplicatedStorage.State.Voting.Enabled --Directory to the skip
-				Prop:GetPropertyChangedSignal("Value"):Connect(function() --Credits to mm for the autoskip script
-				if Prop.Value then --Checks if the skip GUI is present (or if the wave can be skipped)
-				if ActualDirectory.Text == "✓" then --Checks if it's on
- 				game.ReplicatedStorage.RemoteEvent:FireServer("Waves","Skip") --Skips the wave
-			end
-		end
-	end)
-end
-
-ActualDirectory:GetPropertyChangedSignal("Text"):Connect(CheckForTick) --Checks if the checkbox is ticked or not, then runs the script from line 7
+local Prop = game.ReplicatedStorage.State.Voting.Enabled
+Prop:GetPropertyChangedSignal("Value"):Connect(function()
+    if Prop.Value and ActualDirectory.Text == "✓" then
+        game.ReplicatedStorage.RemoteEvent:FireServer("Waves","Skip")
+    end
+end)
